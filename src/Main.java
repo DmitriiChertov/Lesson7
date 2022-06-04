87d6bccf2b77bacf11d3d4ef6d7cd89c25f3deaa
Dmitrii <chertov.dmitrii@yandex.ru>
2022-06-04 20:36:21 +0300

    Четвёртый комит.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String phone = "+9604157537";
         phone = phone.replace("-", "");
-        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
         phone = phone.replace("+", "");
         if (phone.length() == 10) {
 //            phone = '7' + phone;
@@ -21,6 +21,17 @@ public class Main {
             System.out.println("Успех");
         } else {
             System.out.println("Неудача");
+        }
+        System.out.println();
+        String firsrName = "Семён" + " ";
+        String middleName = "Семёнович" + " ";
+        String lastName = "Иванов" + " ";
+        String fullName = firsrName + middleName + lastName;
+        String uppFullName = fullName.toUpperCase();
+        fullName = fullName.replace("ё", "е");
+        {
+            System.out.println("Данные ФИО сотрудника - " + fullName);
+            System.out.println(uppFullName);
         }
     }
 }
